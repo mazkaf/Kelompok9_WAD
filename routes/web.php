67a66ef1a5daa5307cdaa4e024ca194439469948
Diandra Kezia <diandrakezia@gmail.com>
2025-06-15 09:26:52 +0700

    Commit Route

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PasienController;
+use App\Http\Controllers\UserController;
 
 Route::resource('pasien', PasienController::class);
+
+
+
+//User
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
